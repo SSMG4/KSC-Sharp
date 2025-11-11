f6f8816fd3d168b47cf7f2cf6706e0aa96b6349f
SSMG4 <matthew.gallon0912@gmail.com>
2025-11-12 00:37:02 +0100

    Create UriParser.cs

---
+++ KoroneStrap.Core/UriParser.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace KSCSharp.Core;
+
+public class ParsedUri
+{
+    public List<string> Args { get; init; } = new();
+    public string ArgsString { get; init; } = string.Empty;
+    public string Year { get; init; } = "2017L";
+}
+
+public static class UriParser
+{
+    private static readonly Dictionary<string, string> UriKeyArgMap = new()
+    {
+        ["launchmode"] = "--",
+        ["gameinfo"] = "-t",
+        ["placelauncherurl"] = "-j",
+        ["launchtime"] = "--launchtime=",
+        ["task"] = "-task",
+        ["placeId"] = "-placeId",
+        ["universeId"] = "-universeId",
+        ["userId"] = "-userId",
+    };
+
+    // Input expects something like "launchmode:Some+placeId:1234+clientversion:2018"
+    public static ParsedUri Parse(string raw)
+    {
+        var parsed = new ParsedUri();
+        var parts = raw.Split('+', StringSplitOptions.RemoveEmptyEntries);
+        var argList = new List<string>();
+        var argStrPieces = new List<string>();
+        var year = "2017L";
+
+        foreach (var part in parts)
+        {
+            if (!part.Contains(':')) continue;
+            var idx = part.IndexOf(':');
+            var key = part.Substring(0, idx);
+            var val = part.Substring(idx + 1);
+
+            if (key == "clientversion" && !string.IsNullOrEmpty(val))
+            {
+                year = val;
+                continue;
+            }
+
+            if (!UriKeyArgMap.ContainsKey(key) || string.IsNullOrEmpty(val))
+                continue;
+
+            if (key == "placelauncherurl")
+            {
+                val = HttpUtility.UrlDecode(val);
+            }
+
+            var prefix = UriKeyArgMap[key];
+            if (key == "launchmode")
+            {
+                // launchmode special: add negotiation URL as in Python
+                argList.Add($"{prefix}{val}");
+                argList.Add("-a");
+                argList.Add("https://www.pekora.zip/Login/Negotiate.ashx");
+
+                argStrPieces.Add($"{prefix}{val}");
+                argStrPieces.Add("-a https://www.pekora.zip/Login/Negotiate.ashx");
+            }
+            else
+            {
+                if (prefix.EndsWith("="))
+                {
+                    argList.Add($"{prefix}{val}");
+                    argStrPieces.Add($"{prefix}{val}");
+                }
+                else
+                {
+                    argList.Add(prefix);
+                    argList.Add(val);
+                    argStrPieces.Add($"{prefix}{val}");
+                }
+            }
+        }
+
+        parsed.Args = argList;
+        parsed.ArgsString = string.Join(" ", argStrPieces);
+        parsed.Year = year;
+        return parsed;
+    }
+}
